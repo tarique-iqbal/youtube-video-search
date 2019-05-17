3b19d9b7867cabf260197cc2ef97a76c4f9bbb3f
Tarique Iqbal <live.iqbal@gmail.com>
2019-05-17 18:38:43 +0200

    Add index.php file

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types = 1);
+
+use YoutubeVideoSearch\Container\ContainerFactory;
+
+ini_set('error_reporting', 'E_ALL');
+ini_set('display_errors', 'On');
+
+const BASE_DIR = __DIR__;
+
+if (php_sapi_name() !== 'cli') {
+    die('Please run the application from command line.');
+}
+
+require_once BASE_DIR . '/vendor/autoload.php';
+$config = include BASE_DIR . '/config/parameters.php';
+
+$container = (new ContainerFactory($config))->create();
+
+set_exception_handler([$container['ExceptionHandler'], 'report']);
+
+$container['YoutubeVideoSearchApplication']->search();
