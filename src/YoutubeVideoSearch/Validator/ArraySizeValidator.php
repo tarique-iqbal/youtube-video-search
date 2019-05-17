4f42d983aea3e0e2f79b164a1e385e7321e5e8a9
Tarique Iqbal <live.iqbal@gmail.com>
2019-05-17 17:32:53 +0200

    Implement ArraySizeValidator class

---
+++ src/YoutubeVideoSearch/Validator/ArraySizeValidator.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types = 1);
+
+namespace YoutubeVideoSearch\Validator;
+
+/**
+ * Class ArraySizeValidator
+ * @package LanguageGroup\Validator
+ */
+class ArraySizeValidator
+{
+    /**
+     * @var string
+     */
+    private $errorMessage;
+
+    /**
+     * @param array $array
+     * @param int $inputSize
+     * @return bool
+     */
+    public function isValid(array $array, int $inputSize): bool
+    {
+        if (count($array) !== $inputSize) {
+            $this->errorMessage = 'Invalid input length.';
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @return string
+     */
+    public function getErrorMessage(): string
+    {
+        return $this->errorMessage;
+    }
+}
---
+++ tests/Unit/Validator/ArraySizeValidatorTest.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Unit\Service;
+
+use PHPUnit\Framework\TestCase;
+use YoutubeVideoSearch\Validator\ArraySizeValidator;
+
+class ArraySizeValidatorTest extends TestCase
+{
+    public function addArraySizeDataProvider()
+    {
+        return [
+            [
+                [], 1, false
+            ],
+            [
+                ['', ''], 1, false
+            ],
+            [
+                ['Key', 'Search key', 'word'], 1, false
+            ],
+            [
+                ['MLP'], 1, true
+            ],
+            [
+                ['Long search keyword'], 1, true
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider addArraySizeDataProvider
+     */
+    public function testIsValid(array $array, int $expectedInputSize, bool $expectedStatus): void
+    {
+        $arraySizeValidator = new ArraySizeValidator();
+        $status = $arraySizeValidator->isValid($array, $expectedInputSize);
+
+        $this->assertSame($expectedStatus, $status);
+    }
+}
